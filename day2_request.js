422e6bb2d3b9e964633d02380a2263e88d672a36
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-07-17 17:38:04 +0530

    File Update

--- day2_request.js
+++ day2_request.js
@@ -2,4 +2,13 @@
 // Request 1 - Write a script that performs the basic arithmetic operations on two numbers and logs the results.
 const Req1 = "\nRequest 1 Output :- \n";
 console.log(Req1); 
+let Num1,Num2,Add,Sub,Mul,Div,Rem,Result;
+// Addition
 
+// Substraction
+
+// Multiplication
+
+// Division
+
+// Remainder
