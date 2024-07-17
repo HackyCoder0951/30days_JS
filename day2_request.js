75c57dc82b0d31ab0dbb47a3285b723ddab86104
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-07-17 18:11:49 +0530

    File Update

--- day2_request.js
+++ day2_request.js
@@ -2,20 +2,37 @@
 // Request 1 - Write a script that performs the basic arithmetic operations on two numbers and logs the results.
 const Req1 = "\nRequest 1 Output :- \n";
 console.log(Req1); 
+
 let Num1 = 4.90, Num2 = 50;
 let Add, Sub, Mul, Div, Rem, Result;
+
 // Addition
 Add = Num1 + Num2;
 console.log(`Addition       of ${Num1} and ${Num2} is : ${Add}`);
+
 // Substraction
 Sub = Num1 - Num2;
 console.log(`Substration    of ${Num1} and ${Num2} is : ${Sub}`);
+
 // Multiplication
 Mul = Num1 * Num2;
 console.log(`Multiplication of ${Num1} and ${Num2} is : ${Mul}`);
+
 // Division
 Div = Num1 / Num2;
 console.log(`Division       of ${Num1} and ${Num2} is : ${Div}`);
+
 // Remainder
 Rem = Num1 % Num2;
-console.log(`Remainder      of ${Num1} and ${Num2} is : ${Rem}`);
+console.log(`Remainder      of ${Num1} and ${Num2} is : ${Rem}\n`);
+
+
+// Request 2 - Write a script that performs the Comparision & Logical Operation on two numbers and logs the results.
+const Req2 = "Request 2 Output :- \n";
+console.log(Req2); 
+
+let num1, num2;
+let LessThen, GreaterThen, LessEqual, GreaterEqual, EqualityCheck;
+let LogicalAnd, LogicalOR, LogicalNot;
+
+// 
