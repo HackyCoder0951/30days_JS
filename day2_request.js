76177117e825dc1545b7171547fe4570663a631d
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-07-17 17:44:28 +0530

    File Update

--- day2_request.js
+++ day2_request.js
@@ -2,13 +2,20 @@
 // Request 1 - Write a script that performs the basic arithmetic operations on two numbers and logs the results.
 const Req1 = "\nRequest 1 Output :- \n";
 console.log(Req1); 
-let Num1,Num2,Add,Sub,Mul,Div,Rem,Result;
+let Num1 = 4.90, Num2 = 50;
+let Add, Sub, Mul, Div, Rem, Result;
 // Addition
-
+Add = Num1 + Num2;
+console.log(`Addition       of ${Num1} and ${Num2} is : ${Add}`);
 // Substraction
-
+Sub = Num1 - Num2;
+console.log(`Substration    of ${Num1} and ${Num2} is : ${Sub}`);
 // Multiplication
-
+Mul = Num1 * Num2;
+console.log(`Multiplication of ${Num1} and ${Num2} is : ${Mul}`);
 // Division
-
+Div = Num1 / Num2;
+console.log(`Division       of ${Num1} and ${Num2} is : ${Div}`);
 // Remainder
+Rem = Num1 % Num2;
+console.log(`Remainder      of ${Num1} and ${Num2} is : ${Rem}`);
