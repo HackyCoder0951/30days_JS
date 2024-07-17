81125334de40438b462cf8fa567fc0d45cb2a525
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-07-17 18:40:06 +0530

    File Update

--- day3.js
+++ day3.js
@@ -2,3 +2,22 @@
 
 /* Day 3 - Control Structures */
 
+// Activity 1 : If-Else Statements
+console.log("Activity 1 : If-Else Statements :- ");
+// Task 01 - WAP to
+
+// Activity 2 : Nested If-Else Statements
+console.log("Activity 2 : Nested If-Else Statements :- ");
+// Task 01 - WAP to
+
+// Activity 3 : Switch Case Statements
+console.log("Activity 3 : Switch Case Statements :- ");
+// Task 01 - WAP to
+
+// Activity 4 : Conditional(Ternary) Operator
+console.log("Activity 4 : Conditional(Ternary) Operator :- ");
+// Task 01 - WAP to
+
+// Activity 5 : Combining Conditions
+console.log("Activity 5 : Combining Conditions :- ");
+// Task 01 - WAP to
