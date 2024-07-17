24b3827625ea0f1cc4a782fc27714c2e242f61a9
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-07-17 17:35:29 +0530

    File Update

---
+++ day2_request.js
@@ -0,0 +1,5 @@
+'use strict';
+// Request 1 - Write a script that performs the basic arithmetic operations on two numbers and logs the results.
+const Req1 = "\nRequest 1 Output :- \n";
+console.log(Req1); 
+
