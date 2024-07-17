c7250bb08f37039a3103b048164a4cd547df03e6
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-07-17 18:40:58 +0530

    File Update

--- day3.js
+++ day3.js
@@ -5,19 +5,21 @@
 // Activity 1 : If-Else Statements
 console.log("Activity 1 : If-Else Statements :- ");
 // Task 01 - WAP to
+// Task 02 - WAP to
 
 // Activity 2 : Nested If-Else Statements
 console.log("Activity 2 : Nested If-Else Statements :- ");
-// Task 01 - WAP to
+// Task 03 - WAP to
 
 // Activity 3 : Switch Case Statements
 console.log("Activity 3 : Switch Case Statements :- ");
-// Task 01 - WAP to
+// Task 04 - WAP to
+// Task 05 - WAP to
 
 // Activity 4 : Conditional(Ternary) Operator
 console.log("Activity 4 : Conditional(Ternary) Operator :- ");
-// Task 01 - WAP to
+// Task 06 - WAP to
 
 // Activity 5 : Combining Conditions
 console.log("Activity 5 : Combining Conditions :- ");
-// Task 01 - WAP to
+// Task 07 - WAP to
