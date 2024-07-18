749eea656532c66cb0655ae62d4cafa4008c6629
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-07-18 16:13:47 +0530

    Tasks Added in day4.js
    Signed-off-by: @_Hacky_Coder <jignesh.vision.17@gmail.com>

--- day4.js
+++ day4.js
@@ -3,19 +3,25 @@
 /* Day 4 - Loops */
 // Activity 1 : For Loop
 console.log("\nActivity 1 : For Loop :- \n");
+// Task 01 - WAP to print numbers from 1 to 10 using a for loop
+// Task 02 - WAP to print the multiplication table of 5 using a for loop
 
 
 // Activity 2 : While Loop
 console.log("\nActivity 2 : While Loop :- \n");
-
+// Task 03 - WAP to calculate the sum of numbers from 1 to 10 using while loop
+// Task 04 - WAP to print numbers from 10 to 1 using a while loop
 
 // Activity 3 : Do...While Loop
 console.log("\nActivity 3 : Do...While Loop :- \n");
-
+// Task 05 - WAP to print numbers from 1 to 5 using a do...while loop
+// Task 06 - WAP to calculate the factorial of a number using a do...while loop
 
 // Activity 4 : Nested Loops
 console.log("\nActivity 4 : Nested Loops :- \n");
-
+// Task 07 - WAP to print a pattern using nested for loop
 
 // Activity 5 : Loop Control Statement
 console.log("\nActivity 5 : Loop Control Statement :- \n");
+// Task 08 - WAP to print numbers from 1 to 10, but skip the number 5 using the continue statement
+// Task 09 - WAP to print numbers from 1 to 10, but stop the loop when the number is 7 using the break statement
