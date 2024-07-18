f40e9766ab384b7598df3ffd1679ddaa20965cdf
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-07-18 15:52:23 +0530

    File day4.js Updated
    Signed-off-by: @_Hacky_Coder <jignesh.vision.17@gmail.com>

--- day4.js
+++ day4.js
@@ -1,3 +1,21 @@
 "use strict";
 
 /* Day 4 - Loops */
+// Activity 1 : For Loop
+console.log("\nActivity 1 : For Loop :- \n");
+
+
+// Activity 2 : While Loop
+console.log("\nActivity 2 : While Loop :- \n");
+
+
+// Activity 3 : Do...While Loop
+console.log("\nActivity 3 : Do...While Loop :- \n");
+
+
+// Activity 4 : Nested Loops
+console.log("\nActivity 4 : Nested Loops :- \n");
+
+
+// Activity 5 : Loop Control Statement
+console.log("\nActivity 5 : Loop Control Statement :- \n");
