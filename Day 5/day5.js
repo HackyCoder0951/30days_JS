0393dc1a7befc205673931d3a6524079b30a6bd5
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-07-18 19:18:01 +0530

    Activity Added in "Day
    Signed-off-by: @_Hacky_Coder <jignesh.vision.17@gmail.com>

---
+++ Day 5/day5.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/* Day 5 - Functions */
+
+// Activity 1 : Function Declaration
+console.log("Activity 1 : Function Declaration :- ");
+// Task 01 - 
+
+// Activity 2 : Function Expression
+console.log("Activity 2 : Function Expression :- ");
+// Task 01 - 
+
+// Activity 3 : Arrow Function
+console.log("Activity 3 : Arrow Function :- ");
+// Task 01 - 
+
+// Activity 4 : Function Parameters and Default Values
+console.log("Activity 4 : Function Parameters and Default Values :- ");
+// Task 01 - 
+
+// Activity 5 : Higher-Order Function
+console.log("Activity 5 : Higher-Order Function :- ");
+// Task 01 - 
