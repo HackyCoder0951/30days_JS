335094a756ebda0478b4919db63e7459c86458f7
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-07-18 18:37:21 +0530

    Request 5 is added to Day3/
    Signed-off-by: @_Hacky_Coder <jignesh.vision.17@gmail.com>

---
+++ Day3/day3_request.js
@@ -0,0 +1,4 @@
+'use strict';
+// Request 1 - Write a script that check if a number is positive, negative, or zero and log the results.
+const Req1 = "\nRequest 1 Output :- \n";
+console.log(Req1); 
