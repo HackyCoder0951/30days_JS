3997682f83bee7124e6853581c48636af76f589d
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-07-18 18:16:21 +0530

    New File day4_request.js Added
    Signed-off-by: @_Hacky_Coder <jignesh.vision.17@gmail.com>

---
+++ day4_request.js
@@ -0,0 +1,20 @@
+'use strict';
+// Request 1 - WAS that prints numbers from 1 to 10 using a for loop and a while loop.
+const Req1 = "\nRequest 1 Output :- \n";
+console.log(Req1); 
+
+// Request 2 - WAS that prints multiplication table of 5 using a for loop.
+const Req2 = "\nRequest 2 Output :- \n";
+console.log(Req1); 
+
+// Request 3 - WAS that prints a pattern of stars using nested loops.
+const Req3 = "\nRequest 3 Output :- \n";
+console.log(Req3); 
+
+// Request 4 - WAS that calculates the sum of numbers from 1 to 10 using a while loop.
+const Req4 = "\nRequest 4 Output :- \n";
+console.log(Req4); 
+
+// Request 5 - WAS that calculates the factorial of a number using a do...while loop.
+const Req5 = "\nRequest 5 Output :- \n";
+console.log(Req5); 
