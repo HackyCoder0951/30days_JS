168b9f2f661b11a935e83f237e473ddabc5c87e9
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-07-18 16:36:11 +0530

    Task 2 Complete in day4.js
    Signed-off-by: @_Hacky_Coder <jignesh.vision.17@gmail.com>

--- day4.js
+++ day4.js
@@ -4,19 +4,23 @@
 // Activity 1 : For Loop
 console.log("\nActivity 1 : For Loop :- \n");
 // Task 01 - WAP to print numbers from 1 to 10 using a for loop
+console.log("Task 1 - The Numbers from 1 to 10 :- \n");
 for (let i = 1; i <= 10; i++) {
     console.log(`${i}`);
 }
-// Task 02 - WAP to print the multiplication table of 5 using a for loop
-for (j = 5; j <= j; j++) {
-    console.log(`${j}`);
+// Task 02 - WAP to print the multiplication table of 5 using a for loop\
+console.log("\nTask 2 - The Table of 5 :- \n");
+for (let j = 1; j <= 10; j++) {
+    console.log(`5 * ${j} = ${5 * j}`);
 }
 
 // Activity 2 : While Loop
 console.log("\nActivity 2 : While Loop :- \n");
 // Task 03 - WAP to calculate the sum of numbers from 1 to 10 using while loop
 
+
 // Task 04 - WAP to print numbers from 10 to 1 using a while loop
+
 
 // Activity 3 : Do...While Loop
 console.log("\nActivity 3 : Do...While Loop :- \n");
