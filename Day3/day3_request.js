8e3d8bda2b0e1ee71a256a7150d01f4481382cfb
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-07-18 18:45:12 +0530

    File Structure Update
    Signed-off-by: @_Hacky_Coder <jignesh.vision.17@gmail.com>

--- Day3/day3_request.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-// Request 1 - Write a script that check if a number is positive, negative, or zero and log the results.
-const Req1 = "\nRequest 1 Output :- \n";
-console.log(Req1); 
