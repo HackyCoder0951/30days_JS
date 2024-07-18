3fc67feac3409e8143036cc373ff5a59053d41ba
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-07-18 13:43:17 +0530

    commit message - day3.js
    Signed-off-by: @_Hacky_Coder <jignesh.vision.17@gmail.com>

--- day3.js
+++ day3.js
@@ -4,22 +4,26 @@
 
 // Activity 1 : If-Else Statements
 console.log("Activity 1 : If-Else Statements :- ");
-// Task 01 - WAP to
-// Task 02 - WAP to
+// Task 01 - WAP to check if a number is positive, negative, or zero and log the results.
+
+
+// Task 02 - WAP to check if a person is eligible to vote ( age > = 18) and log the results.
 
 // Activity 2 : Nested If-Else Statements
 console.log("Activity 2 : Nested If-Else Statements :- ");
-// Task 03 - WAP to
+// Task 03 - WAP to find the largest three numbers using nested if-else statements.
 
 // Activity 3 : Switch Case Statements
 console.log("Activity 3 : Switch Case Statements :- ");
-// Task 04 - WAP to
-// Task 05 - WAP to
+// Task 04 - WAP that uses a switch case to determine the day of the week based on a number (1-7) and log the day name to the console. 
+
+
+// Task 05 - WAP that uses a switch case to assign a grade ('A','B','C','D','F') based on score and log the grade to the console.
 
 // Activity 4 : Conditional(Ternary) Operator
 console.log("Activity 4 : Conditional(Ternary) Operator :- ");
-// Task 06 - WAP to
+// Task 06 - WAP that uses the ternary operator to check if a number is even or odd and log the results. 
 
 // Activity 5 : Combining Conditions
 console.log("Activity 5 : Combining Conditions :- ");
-// Task 07 - WAP to
+// Task 07 - WAP to check if a year is a leap year using multiple conditions ( divisible by 4, but not 100 unless also divisible by 400) and log the results.
