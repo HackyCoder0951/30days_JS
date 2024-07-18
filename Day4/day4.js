9e8f68ac09f78e0afc5e8c3a56d771ab8e30e3d8
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-07-18 18:37:24 +0530

    Request 5 is added to Day4/
    Signed-off-by: @_Hacky_Coder <jignesh.vision.17@gmail.com>

---
+++ Day4/day4.js
@@ -0,0 +1,90 @@
+"use strict";
+
+/* Day 4 - Loops */
+// Activity 1 : For Loop
+console.log("\nActivity 1 : For Loop :- \n");
+// Task 01 - WAP to print numbers from 1 to 10 using a for loop
+console.log("Task 1 - The Numbers from 1 to 10 :- \n");
+for (let i = 1; i <= 10; i++) {
+    console.log(`${i}`);
+}
+// Task 02 - WAP to print the multiplication table of 5 using a for loop\
+console.log("\nTask 2 - The Table of 5 :- \n");
+for (let j = 1; j <= 10; j++) {
+    console.log(`5 * ${j} = ${5 * j}`);
+}
+
+// Activity 2 : While Loop
+console.log("\nActivity 2 : While Loop :- ");
+// Task 03 - WAP to calculate the sum of numbers from 1 to 10 using while loop
+console.log("\nTask 3 - The Sum of Numbers from 1 to 10 :- \n");
+let Num = 1;
+let Sum = 0;
+while (Num <= 10){
+    Sum += Num;
+    Num++;
+}
+console.log(`The Sum of Numbers from 1 to 10 is : ${Sum}`);
+
+// Task 04 - WAP to print numbers from 10 to 1 using a while loop
+console.log("\nTask 4 - Print numbers from 10 to 1 :- ");
+let k = 10;
+while (k >= 1){
+    console.log(`${k}`);
+    k--;
+}
+
+// Activity 3 : Do...While Loop
+console.log("\nActivity 3 : Do...While Loop :- \n");
+// Task 05 - WAP to print numbers from 1 to 5 using a do...while loop
+console.log("\nTask 5 - Print numbers from 1 to 5 :- \n");
+let l = 1;
+do {
+  console.log(`${l}`);
+  l++;
+} while (l <= 5);
+
+
+// Task 06 - WAP to calculate the factorial of a number using a do...while loop
+console.log("\nTask 6 - Calculate the factorial of a number :- \n");
+let num = 5;
+let factorial = 1;
+let a = 1;
+do {
+    factorial *= a;
+    a++;
+} while (a <= num);
+console.log(`The factorial of ${num} is ${factorial}`);
+
+// Activity 4 : Nested Loops
+console.log("\nActivity 4 : Nested Loops :- \n");
+// Task 07 - WAP to print a pattern using nested for loop
+console.log("\nTask 7 - Print a pattern using nested for loop :- \n");
+const rows = 5;
+for (let i = 0; i < rows; i++) {
+    let str = "";
+    for (let j = 0; j <= i; j++) {
+        str += "*";
+    }
+    console.log(`${str}`);
+}
+
+// Activity 5 : Loop Control Statement
+console.log("\nActivity 5 : Loop Control Statement :- \n");
+// Task 08 - WAP to print numbers from 1 to 10, but skip the number 5 using the continue statement
+console.log("\nTask 8 - Print numbers from 1 to 10, but skip the number 5 using the continue statement :- \n");
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) {
+        continue; // Skip number 5
+    }
+    console.log(`${i}`);
+}
+
+// Task 09 - WAP to print numbers from 1 to 10, but stop the loop when the number is 7 using the break statement
+console.log("\nTask 9 - Print numbers from 1 to 10, but stop the loop when the number is 7 using the break statement :- \n");
+for (let i = 1; i <= 10; i++) {
+    if (i === 7) {
+        break; // stoped at number 7
+    }
+    console.log(`${i}`);
+}
---
+++ Day4/day4_request.js
@@ -0,0 +1,59 @@
+'use strict';
+// Request 1 - WAS that prints numbers from 1 to 10 using a for loop and a while loop.
+const Req1 = "\nRequest 1 Output :-";
+console.log(Req1); 
+console.log("\nUsing For Loop :- \n");
+for (let i = 1; i <= 10; i++) {
+    console.log(`${i}`);
+}
+console.log("\nUsing While Loop :- \n");
+let i = 1;
+while(i <= 10) {
+    console.log(`${i}`);
+    i++;
+}
+
+// Request 2 - WAS that prints multiplication table of 5 using a for loop.
+const Req2 = "\nRequest 2 Output :-";
+console.log(Req2); 
+console.log("\nThe Table of 5 :- \n");
+for (let j = 1; j <= 10; j++) {
+    console.log(`5 * ${j} = ${5 * j}`);
+}
+
+// Request 3 - WAS that prints a pattern of stars using nested loops.
+const Req3 = "\nRequest 3 Output :-";
+console.log(Req3); 
+console.log("\nPrint a pattern using nested for loop :- \n");
+const rows = 5;
+for (let i = 0; i < rows; i++) {
+    let str = "";
+    for (let j = 0; j <= i; j++) {
+        str += "*";
+    }
+    console.log(`${str}`);
+}
+
+// Request 4 - WAS that calculates the sum of numbers from 1 to 10 using a while loop.
+const Req4 = "\nRequest 4 Output :-";
+console.log(Req4); 
+let Num = 1;
+let Sum = 0;
+while (Num <= 10){
+    Sum += Num;
+    Num++;
+}
+console.log(`\nThe Sum of Numbers from 1 to 10 is : ${Sum}`);
+
+// Request 5 - WAS that calculates the factorial of a number using a do...while loop.
+const Req5 = "\nRequest 5 Output :-";
+console.log(Req5); 
+console.log("\nCalculate the factorial of a number :- \n");
+let num = 5;
+let factorial = 1;
+let a = 1;
+do {
+    factorial *= a;
+    a++;
+} while (a <= num);
+console.log(`The factorial of ${num} is ${factorial}`);
