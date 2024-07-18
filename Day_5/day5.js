c2b7e9629a76c27054d3eb3dcd14869359c378fd
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-07-18 20:29:47 +0530

    File Day_5/day5.js Updated
    Signed-off-by: @_Hacky_Coder <jignesh.vision.17@gmail.com>

--- Day_5/day5.js
+++ Day_5/day5.js
@@ -4,20 +4,44 @@
 
 // Activity 1 : Function Declaration
 console.log("Activity 1 : Function Declaration :- ");
-// Task 01 - 
+// Task 01 - WAF to check if a number is even or odd and log the results.
+function checkEvenOrOdd(number) {
+    if (number % 2 === 0) {
+        console.log(`The Given Number ${number} is even.`);
+    } else {
+        console.log(`The Given Number ${number} is odd.`);
+    }
+}
+checkEvenOrOdd(4);
+checkEvenOrOdd(-57);
+
+// Task 02 - WAF to calculate the square of a number and log the results.
+function calculateSquare(number) {
+    const Square = number * number ;
+    console.log(`The Square of Given Number ${Square}`);
+}
+calculateSquare(5);
+calculateSquare(8); 
 
 // Activity 2 : Function Expression
 console.log("Activity 2 : Function Expression :- ");
-// Task 01 - 
+// Task 03 - WAF expression to find the maximum of two numbers and log the result to the console.
+// Task 04 - WAF expression to concatenate two strings and return the results.
+
 
 // Activity 3 : Arrow Function
 console.log("Activity 3 : Arrow Function :- ");
-// Task 01 - 
+// Task 05 - WAAF to calculate the sum of two numbers and log the results.
+// Task 06 - WAAF to check if a strings contains a specific character and return a boolean value.
 
 // Activity 4 : Function Parameters and Default Values
 console.log("Activity 4 : Function Parameters and Default Values :- ");
-// Task 01 - 
+// Task 07 - WAF that takes two parameters and returns their product. Provide a default value for the second parameter.
+// Task 08 - WAF that takes a person's name and age and returns a greeting message. Provide a default value for the age.
+- 
+
 
 // Activity 5 : Higher-Order Function
 console.log("Activity 5 : Higher-Order Function :- ");
-// Task 01 - 
+// Task 09 - WAF higher-order function that takes a function and a number, and calls the functions that many times.
+// Task 10 - WAF higher-order function that takes two functions and a value, applies the first function to the value, and then applies the second function to the result.
